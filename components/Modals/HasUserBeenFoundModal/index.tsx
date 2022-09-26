d70a70b5870ddc4254a5207ea1c138c34cfde8e8
LeonardoFariaOliveira <leonardoN00Bfaria@hotmail.com>
2022-09-25 21:29:34 -0300

    Added more z-index to HasUserBeenFoundModal

--- components/Modals/HasUserBeenFoundModal/index.tsx
+++ components/Modals/HasUserBeenFoundModal/index.tsx
@@ -14,7 +14,7 @@ export default function HasUserBeenFoundModal({latitude, longitude, setHasUserBe
 
     return(
         <>
-            <div className="bg-light-green slide-on-modal absolute z-10 md:top-[9rem] top-[20rem] right-1 md:right-3 rounded-[4px] py-5 pl-2 md:pl-6 pr-0 mb-4 max-w-[17rem] md:max-w-[24rem]  w-full text-base border-green border text-black" role="alert">
+            <div className="bg-light-green slide-on-modal absolute z-[999] md:top-[9rem] top-[20rem] right-1 md:right-3 rounded-[4px] py-5 pl-2 md:pl-6 pr-0 mb-4 max-w-[17rem] md:max-w-[24rem]  w-full text-base border-green border text-black" role="alert">
                 <div className="flex-row flex justify-between pr-5">
                     <h4 className="text-md font-medium leading-tight mb-4">Ei encontramos você!</h4>
                     <div
